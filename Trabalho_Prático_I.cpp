a9d0cf35003cbd0f8981861b8c9a843824c3e380
Albert Medeiros De Paula <201811000016@IFSMP.PAS.IFSULDEMINAS.EDU.BR>
2020-03-11 10:34:45 -0300

    c1 - criando estrutura base e editando pdf

---
+++ Trabalho_Prático_I.cpp
@@ -0,0 +1,42 @@
+#include<stdlib.h>
+#include<stdio.h>
+
+#define tam 51
+
+void inicializaGrafos(){
+
+}
+
+void inserirUsuario(){
+
+}
+
+void inserirRelacao(){
+
+}
+
+void listarSeguidores(){
+
+}
+
+void listarSeguidoresVelhos(){
+
+}
+
+void atualizarRelacao(){
+
+}
+
+void removerUsuario(){
+
+}
+
+void removerRelacao(){
+
+}
+
+
+main(){
+	
+	
+}
